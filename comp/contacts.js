d2e3d097c1fec57e5f65d5c5849f05fdbc3d96ba
banerjeeupasana003 <banerjeeupasana03@gmail.com>
2019-10-09 10:10:39 -0700

    added content in contacts page

---
+++ comp/contacts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+
+
+function Contacts(){
+  return (
+    <View>
+      <Text>
+        This is the contacts page
+      </Text>
+    </View>
+  )
+}
+
+export default Contacts;
