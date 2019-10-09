b0e2a3e949d7d8a648f5228681c492ffaf86e0c2
HidekiMurata <ilovrbed0324@gmail.com>
2019-10-09 10:16:22 -0700

    contacts.js has changed firlst time
    type a little bit

---
+++ comp/contacts.js
@@ -0,0 +1,16 @@
+import react from ' react';
+import {View, Text} from 'react-native';
+import styles from './styles/styles.js';
+
+
+function App(){
+    return(
+        <View>
+            <Text>Contact Page</Text>
+        </View>
+    )
+}
+
+
+
+export default contact;
