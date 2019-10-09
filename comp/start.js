a12f7f4951ae3e3c17fa3a30df90027b604b90dc
banerjeeupasana003 <banerjeeupasana03@gmail.com>
2019-10-09 09:47:51 -0700

    added first line of text content in start js

---
+++ comp/start.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, Image} from 'react-native';
+
+
+function App(){
+  return (
+    <View>
+      <Text>
+        Welcome to group collaboration Hideki, Han and Upasana!
+      </Text>
+    </View>
+  )
+}
+
+export default App;
